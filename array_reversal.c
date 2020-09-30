e9b7127d3aa07afaf67b52080a3c12d311859697
Akshat1609 <65245150+Akshat1609@users.noreply.github.com>
2020-09-30 19:17:13 +0530

    Update array_reversal.c

--- array_reversal.c
+++ array_reversal.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+//Simply traversing in reverse is not efficient way
 int main()
 {
-     int n,i,arr[1000];
-     scanf("%d",&n);
-     for(i=0;i<n;i++)
+     int n,i,start,num,temp,arr[1000];
+     scanf("%d",&num);
+     for(i=0;i<num;i++)
      {
          scanf("%d",&arr[i]);
      }
-     for(i=n-1;i>=0;i--)
+     //initiaslizing start and end.
+     start=0;
+     n=num;
+     while(start<n-1)
      {
-        printf("%d ",arr[i]);
+        temp=arr[start];
+        arr[start]=arr[n-1];
+        arr[n-1]=temp;
+        start++;
+        n--;
      }
-    
+     for(i=0;i<num;i++)
+     {
+         printf("%d ",arr[i]);
+     }
+     printf("\n");
+
     return 0;
 }
+
 
 
